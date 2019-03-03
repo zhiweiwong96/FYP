20c29aea45179b972f9883a612a1dad6069da795
zhiweiwong96 <zhiwei1996@1utar.my>
2019-03-03 22:48:21 +0800

    part 6 done but it will crash due to NullPointerException

--- app/src/main/java/com/example/zw/fyp/MainActivity.java
+++ app/src/main/java/com/example/zw/fyp/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,30 +21,38 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<String> al;
-    private ArrayAdapter<String> arrayAdapter;
+    private cards cards_data[];
+    private arrayAdapter arrayAdapter;
     private int i;
 
     private FirebaseAuth mAuth;
 
+    private String currentUId;
 
+    private DatabaseReference usersDb;
+
+    ListView listView;
+    List<cards> rowItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        usersDb = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUId = mAuth.getCurrentUser().getUid();
+
         checkUserSex();
 
-        al = new ArrayList<>();
+        rowItems = new ArrayList<cards>();
 
-
-        arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al);
-
-
+        arrayAdapter = new arrayAdapter(this, R.layout.item, rowItems);
 
         SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
 
@@ -53,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
             public void removeFirstObjectInAdapter() {
                 // this is the simplest way to delete an object from the Adapter (/AdapterView)
                 Log.d("LIST", "removed object!");
-                al.remove(0);
+                rowItems.remove(0);
                 arrayAdapter.notifyDataSetChanged();
             }
 
@@ -105,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
                 if(dataSnapshot.getKey().equals(user.getUid())){
                     userSex = "Male";
                     oppUserSex = "Female";
+                    usersDb = usersDb.child(oppUserSex);
                     getOppositeSexUsers();
                 }
             }
@@ -137,6 +147,7 @@ public class MainActivity extends AppCompatActivity {
                 if(dataSnapshot.getKey().equals(user.getUid())){
                     userSex = "Female";
                     oppUserSex = "Male";
+                    usersDb = usersDb.child(oppUserSex);
                     getOppositeSexUsers();
                 }
             }
@@ -169,7 +180,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 if(dataSnapshot.exists()){
-                    al.add(dataSnapshot.child("name").getValue().toString());
+                    cards item = new cards(dataSnapshot.getKey(), dataSnapshot.child("name").getValue().toString());
+                    rowItems.add(item);
                     arrayAdapter.notifyDataSetChanged();
                 }
             }
---
+++ app/src/main/java/com/example/zw/fyp/cards.java
@@ -0,0 +1,24 @@
+package com.example.zw.fyp;
+
+public class cards {
+    private String userId;
+    private String name;
+    public cards (String userId, String name){
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public String getUserId(){
+        return userId;
+    }
+    public void setUserID(String userID){
+        this.userId=userId;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+}
